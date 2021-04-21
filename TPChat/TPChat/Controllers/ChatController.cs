08d05153d6dce34d8ccd101c6387b93e49111f27
eperia35 <thomas.huet@hotmail.fr>
2021-04-21 12:37:10 +0200

    add tp chat

---
+++ TPChat/TPChat/Controllers/ChatController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TPChat.Models;
+
+namespace TPChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        
+        public static List<Chat> Chats = Chat.GetMeuteDeChats();
+
+
+        // GET: Chat
+        public ActionResult Index()
+        {
+            return View(Chats);
+        }
+
+        // GET: Chat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(Chats.Where(c => c.Id == id).Single());
+        }
+
+        // GET: Chat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View(Chats.Where(c => c.Id == id).Single());
+        }
+
+        // POST: Chat/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var chat = Chats.FirstOrDefault(c => c.Id == id);
+                if (chat != null)
+                {
+                    Chats.Remove(chat);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+
+        }
+    }
+}
---
+++ TPChat/TPChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TPChat.MvcApplication" Language="C#" %>
